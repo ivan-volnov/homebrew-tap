2ee54a6369f24b367e43db430417f470a1228b91
ivan-volnov <ivan-volnov@users.noreply.github.com>
2020-07-04 19:12:17 +0800

    fix sha sum

--- Formula/keybr.rb
+++ Formula/keybr.rb
@@ -2,7 +2,7 @@ class Keybr < Formula
   desc "Advanced console keyboard trainer with language learning support"
   homepage "https://github.com/ivan-volnov/keybr"
   url "https://github.com/ivan-volnov/keybr/releases/download/v1.0/keybr-src-1.0.tar.gz"
-  sha256 "333df431433f01ece827e523cddf3f79742c2603b293c8827d20010fb38fccc2"
+  sha256 "ca83a5b099eafbe621a7b1d59af8d2b936412fc64334beb821ba3d9a059aab21"
   license "MIT"
 
   depends_on "cmake" => :build
